47c56ca89183e512a0c8d2ceb1d36f654f619aa0
Genevieve Perron-Migneron <info@gen-migneron.com>
2020-09-20 21:55:18 -0400

    add category  page

---
+++ components/Category.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="category">
+    <div class="row">
+      <div class="col-md-8">
+        <h1 class="block__title">{{category.title}}</h1>
+        <div v-html="$prismic.asHtml(category.text)" />
+      </div>
+      <div class="col-md-4 category__specs" v-html="$prismic.asHtml(category.specs)" />
+    </div>
+  </div>
+</template>
+<script>
+  import Media from './Media';
+  export default {
+    props: {
+      category: {
+        type: Object,
+        require: true,
+        default: () => ({})
+      }
+    }
+  }
+</script>
---
+++ pages/_about/_categories.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <Hero :hero-data="hero.data" />
+    <section class="categories container">
+      <div class="row align-items-center">
+        <div class="col-md-12">
+          <div v-html="$prismic.asHtml(content.text)" />
+        </div>
+      </div>
+      <div class="categories__items">
+        <template v-for="(item, index) in content.body[0].items">
+          <Category :category="item" :key="index" />
+        </template>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+  import Category from '~/components/Category';
+  import Hero from '~/components/Hero';
+  import Media from '~/components/Media';
+
+  export default {
+    async asyncData({ app, error, store}) {
+      const locale = store.state.locale;
+      let content = []
+      
+      await app.$prismic.api.query(
+        app.$prismic.predicates.at('document.type', 'aboutcategories'), {
+            lang: `${locale}-ca`
+          }
+        ).then((response) => {
+          response.results.forEach(result => {
+            content = result.data;
+          });
+        })
+
+      let hero = {};
+      hero = await app.$prismic.api.getByID(content.hero.id);
+
+      if (content) {
+        return {
+          content,
+          hero
+        }
+      } else {
+        error({ statusCode: 404, message: 'Page not found' })
+      }
+    },
+    head() {
+      return {
+        //title: this.$prismic.asText(this.seo.title),
+        link: [
+        //{ rel: 'canonical', href: `https://<DOMAIN>${this.$prismic.linkResolver(this.document)}` }
+        ],
+        meta: [
+          //{ hid: 'description', name: 'description', content: this.$prismic.asText(this.seo.description) }
+        ]
+      }
+    },
+    components: {
+      Category,
+      Hero,
+      Media
+    },
+    nuxtI18n: {
+      paths: {
+        fr: '/a-propos-du-cannabis/categories',
+        en: '/about-cannabis/categories'
+      }
+    },
+  }
+</script>
